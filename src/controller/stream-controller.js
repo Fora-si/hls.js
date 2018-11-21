cb1be8a1dc79a8e66566b7809c581935aa030499
Jernej Fijacko <jernej.fijacko@fora.si>
2018-11-21 14:05:53 +0100

    Fix same segment quality switch if currentTime is 0

--- src/controller/stream-controller.js
+++ src/controller/stream-controller.js
@@ -331,7 +331,7 @@ class StreamController extends TaskLoop {
         fragEndTime = fragPrevious.endPTS,
         targetDuration = levelDetails.targetduration;
 
-      if (bandwidth && loadLevel && currentTime && previousLevel && loadLevel.bitrate > previousLevel.bitrate) {
+      if (bandwidth && loadLevel && currentTime >= 0 && previousLevel && loadLevel.bitrate > previousLevel.bitrate) {
         var fragSize = loadLevel.bitrate * targetDuration;
         var fragLoadTime = fragSize / bandwidth;
         var timeToLoad = Math.max(0, bufferEnd - currentTime);
