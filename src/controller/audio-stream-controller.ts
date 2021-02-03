d77d9594346be36bc454ddfb46852d50b1596216
Jernej Fijačko <jernej@jernej.org>
2021-02-03 16:31:54 +0100

    Reset next transmuxer timestamp on audio track switch instead on buffer flush

--- src/controller/audio-stream-controller.ts
+++ src/controller/audio-stream-controller.ts
@@ -392,6 +392,10 @@ class AudioStreamController
     } else {
       // switching to audio track, start timer if not already started
       this.setInterval(TICK_INTERVAL);
+      let transmuxer = this.transmuxer;
+      if (transmuxer) {
+        transmuxer.resetNextTimestamp();
+      }
     }
 
     // should we switch tracks ?
