cacca98d9557a95139910f3b5e83fa4c095defa7
Jernej Fijačko <jernej@jernej.org>
2021-02-03 10:05:57 +0100

    Clear previous fragment on fragAborted

--- src/controller/base-stream-controller.ts
+++ src/controller/base-stream-controller.ts
@@ -1085,6 +1085,7 @@ export default class BaseStreamController
       this.log(
         `Fragment ${frag.sn} of level ${frag.level} was aborted, flushing transmuxer`
       );
+      this.fragPrevious = null;
       this.transmuxer.flush(
         new ChunkMetadata(
           frag.level,
