8259e8c1d058df54a86d9254e36e83298e4c86e1
Jernej Fijacko <jernej.fijacko@fora.si>
2022-01-18 14:56:50 +0100

    Use correct currentTime in base stream controller when waiting for startPts and stream was started with a startPosition > 0

--- src/controller/base-stream-controller.ts
+++ src/controller/base-stream-controller.ts
@@ -1884,7 +1884,9 @@ export default class BaseStreamController
     const { media } = this;
     // if we have not yet loaded any fragment, start loading from start position
     let pos = 0;
-    if (this.hls?.hasEnoughToStart && media) {
+    if (this.state == State.WAITING_INIT_PTS && this.lastCurrentTime > 0) {
+      pos = this.lastCurrentTime;
+    } else if (this.hls?.hasEnoughToStart && media) {
       pos = media.currentTime;
     } else if (this.nextLoadPosition >= 0) {
       pos = this.nextLoadPosition;
